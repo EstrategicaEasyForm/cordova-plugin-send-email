82d93329938ba7978d5bc7cf001b8d44184f43af
EstrategicaEasyForm <estrategica.easy.form@gmail.com>
2019-05-16 09:21:51 -0500

    initial commit

---
+++ src/android/com/cordova/smtp/client/SMTPClient.java
@@ -0,0 +1,48 @@
+package com.cordova.smtp.client;
+
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * This class send an email from JavaScript.
+ */
+public class SMTPClient extends CordovaPlugin {
+
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+		try {
+			JSONObject json = new JSONObject(args.getString(0));
+			Mail m = new Mail(json.getString("smtpUserName"), json.getString("smtpPassword"));
+			String[] toArr = json.getString("emailTo").split(",");
+			String emailCC = json.optString("emailCC");
+			String[] ccArr = (emailCC.isEmpty()) ? null : emailCC.split(",");
+			m.set_to(toArr);
+			m.set_cc(ccArr);
+			m.set_host(json.getString("smtp"));
+			m.set_from(json.getString("emailFrom"));
+			m.set_body(json.getString("textBody"));
+			m.set_subject(json.getString("subject"));
+
+			JSONArray attachments = json.getJSONArray("attachments");
+			if (attachments != null) {
+				for (int i = 0; i < attachments.length(); i++) {
+					String fileFullName = attachments.getString(i);
+					if (fileFullName.contains(":")) {
+						fileFullName = fileFullName.split(":")[1];
+					}
+					m.addAttachment(fileFullName);
+				}
+			}
+			m.send();
+			callbackContext.success();
+			return true;
+		
+        } catch (Exception ex) {
+            callbackContext.error(ex.getMessage());
+			return false;
+        }
+    }
+}
