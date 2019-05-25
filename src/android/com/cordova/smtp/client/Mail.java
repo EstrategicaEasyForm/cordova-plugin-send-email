cca715af1a598011757d1e229210d814cadd2b1f
EstrategicaEasyForm <estrategica.easy.form@gmail.com>
2019-05-25 00:46:37 -0500

    add email pdf

--- src/android/com/cordova/smtp/client/Mail.java
+++ src/android/com/cordova/smtp/client/Mail.java
@@ -17,6 +17,7 @@ import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
 import javax.mail.internet.MimeMultipart;
+import javax.mail.util.ByteArrayDataSource;
 
 public class Mail extends javax.mail.Authenticator {
     private String _user;
